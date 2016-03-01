0affe1f6a19a54a3af12fff3fa3376229f726b76
James Borden <jmborden@gmail.com>
2016-03-01 10:53:12 -0600

    confirmation dialogue disappears when another order is made active

--- src/dashboard_cljs/orders.cljs
+++ src/dashboard_cljs/orders.cljs
@@ -25,6 +25,9 @@
                           "servicing"   "complete"
                           "complete"    nil
                           "cancelled"   nil})
+
+(def state (r/atom {:confirming? false}))
+
 (defn EditButton
   "Button for toggling editing? button"
   [editing?]
@@ -337,7 +340,7 @@
   [props]
   (let [error-message (r/atom "")
         retrieving? (r/atom false)
-        confirming? (r/atom false)
+        confirming? (r/cursor state [:confirming?])
         confirm-action (r/atom "")
         confirm-on-click (r/atom false)]
     (fn [{:keys [editing? status order]}
@@ -458,6 +461,7 @@
                                (:id (first couriers)))
             order-status (:status @current-order)
             ]
+        (reset! (r/cursor state [:confirming?]) false)
         ;; create and insert order marker
         (when (:lat @current-order)
           (when @google-marker
@@ -465,10 +469,10 @@
           (reset! google-marker (js/google.maps.Marker.
                                  (clj->js {:position
                                            {:lat (:lat @current-order)
-                                            :lng (:lng @current-order)
-                                            }
-                                           :map (second (get-cached-gmaps :orders))
-                                           }))))
+                                            :lng (:lng @current-order)}
+                                           :map
+                                           (second (get-cached-gmaps :orders))}
+                                          ))))
         ;; populate the current order with additional information
         [:div {:class "panel-body"}
          [:div {:class "row"}
