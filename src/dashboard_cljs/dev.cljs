466383a3f4fe2a691c74e002266a2d4c17e0e637
James <jmborden@gmail.com>
2016-09-28 11:44:28 -0500

    pruned up source tree for lein fighweel development

--- src/dashboard_cljs/dev.cljs
+++ src/dashboard_cljs/dev.cljs
@@ -1,6 +1,6 @@
 (ns dashboard-cljs.dev
-  (:require [weasel.repl :as repl]
-            [dashboard-cljs.core :as core]))
+  (:require [dashboard-cljs.core :as core]))
 
-(when-not (repl/alive?)
-  (repl/connect "ws://127.0.0.1:9001"))
+(defn ^:export on-jsload
+  []
+  (core/init-new-dash))
